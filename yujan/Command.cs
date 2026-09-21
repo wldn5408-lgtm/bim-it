2a54faef6bee6a2f84ee4576ea8d6b29ded5dc63
mongmong2yo2 <mongmong2yo@gmail.com>
2026-09-21 12:15:25 +0900

    나의 첫번째 프로젝트

---
+++ yujan/Command.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.ApplicationServices;
+
+
+namespace yujan
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Hello", "Hello, Revit!");
+            return Result.Succeeded;
+        }
+    }
+}
