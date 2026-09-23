4201cdad5c3b97bd525d6ca9c861496501123d1e
mongmong2yo2 <mongmong2yo@gmail.com>
2026-09-23 13:43:48 +0900

    260923 오전수업

---
+++ yujin/Command.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;  
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.DB.Structure;
+
+namespace yujin
+{
+    [Transaction(TransactionMode.Manual)]   
+    public class Command : IExternalCommand
+    {
+        // Execute가 주문서 이름이다.
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // 단축키를 만든다.
+            UIApplication uiApp = commandData.Application;
+            UIDocument uiDoc = uiApp.ActiveUIDocument;
+            Document doc = uiDoc.Document;
+            // 참조 r은 변수(메모리 저장소의 주소)
+            //Reference r = uiDoc.Selection.PickObject(ObjectType.Element, "객체를 선택하세요");
+            //Element e = doc.GetElement(r); // 선택한 객체를 가져온다.
+
+            IList<Reference> refs = uiDoc.Selection.PickObjects(ObjectType.Edge, "객체를 선택하세요");
+            Face face = doc.GetElement(refs[0]).GetGeometryObjectFromReference(refs[0]) as Face;
+
+            List<Curve> dd = Util.GetCurves(face);
+            //던져주고 받으려면 메모리 공간이 필요하므로 메모리 공간을 만든거임. 그 방 이름이 dd임. 함수를 부르는 방법. =은 대입을 의미함.
+
+
+
+
+
+
+            //List<Curve> curves = new List<Curve>();
+            //  foreach (Reference item in refs) ;
+            //    {
+            //        Edge edge = doc.GetElement(refs[0]).GetGeometryObjectFromReference(refs[0]) as Edge; // edge를 선택한다. edge의 부모로부터 레퍼런스한다.
+
+            //Curve c = edge.AsCurve(); //curve로 변환한다.
+            //    }
+            FilteredElementCollector collecter = new FilteredElementCollector(doc);
+            collecter.OfCategory(BuiltInCategory.OST_StructuralFraming);
+            collecter.OfClass(typeof(FamilySymbol));
+
+            FamilySymbol fs = collecter.FirstElement() as FamilySymbol;
+            Level level = doc.ActiveView.GenLevel;
+
+            foreach (Curve c in dd)
+            {
+                using (Transaction trans = new Transaction(doc, "create beam"))
+                {
+                    trans.Start();
+                    fs.Activate();
+                    FamilyInstance fi = doc.Create.NewFamilyInstance(c, fs, level, StructuralType.Beam);
+                    trans.Commit();
+                }
+            }
+
+            //string a = param.get;
+
+            //Autodesk.Revit.Ui.TaskDialog.Show("코멘트의 정보는 : ", a);
+
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ yujin/Util.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.ApplicationServices;
+
+namespace yujin
+{
+    public class Util
+    {
+        public static List<Curve> GetCurves(Face face)
+        {
+            return null; // null 값을 return한다고 작성해서 GetCurves에 빨간줄이 사라짐.
+            List<Curve> curves = new List<Curve>();
+
+            EdgeArrayArray edgeArrays = face.EdgeLoops;
+            foreach (EdgeArray edgeArray in edgeArrays)
+            {
+                foreach (Edge edge in edgeArray)
+                {
+                    Curve c = edge.AsCurve();
+                    curves.Add(c);
+                }
+            }
+            return curves;
+        }
+    }
+}
